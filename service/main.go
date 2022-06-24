0da141fc06ab7312ca28f2d76fae2adfd8ffad61
Peh <20146907+randomicon00@users.noreply.github.com>
2022-06-24 22:33:16 +0100

    feat: add cors

--- service/main.go
+++ service/main.go
@@ -4,10 +4,24 @@ import (
   "fmt"
   "github.com/gin-gonic/gin"
   "net/http"
+  "github.com/gin-contrib/cors"
 )
 
 func main() {
   r := gin.Default()
+
+  router.Use(cors.New(cors.Config{
+    AllowOrigins:     []string{"https://example.com"},
+    AllowMethods:     []string{"POST", "PUT", "PATCH"},
+    AllowHeaders:     []string{"Origin"},
+    ExposeHeaders:    []string{"Content-Length"},
+    AllowCredentials: true,
+    AllowOriginFunc: func(origin string) bool {
+      return origin == "https://mysite.com"
+    },
+    MaxAge: 60 * time.Minute,
+  }))
+
   r.GET("/hello". func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
       "message": "hello world!",
