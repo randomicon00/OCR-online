729e38bd00df9c5dce77a6b6970537ffa3f5c2c7
Peh <20146907+randomicon00@users.noreply.github.com>
2022-08-10 17:33:54 +0100

    add upload to main app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import Upload from './components/Upload';
+
 
 function App() {
   return (
     <div className="App">
       <p>OCR online conversion homepage!</p>
+      <div>
+        <Upload />
+      </div>
     </div>
   );
 }
