9b3440d9b5cb204f8762451b682b462c646d06ab
Peh <20146907+randomicon00@users.noreply.github.com>
2022-08-21 21:09:20 +0100

    add title and first paragraph

--- front/src/components/Intro.tsx
+++ front/src/components/Intro.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Intro = () => {
   return (
     <div>
-      <p>intro component here...</p>
+      <h1>OCR Image to Text Converter Online</h1>
+      <p>Picture to text converter allows you to extract text from image or convert PDF to Doc, Excel or Text formats
+using Optical Character Recognition software online</p>
     </div>
   );
 }
