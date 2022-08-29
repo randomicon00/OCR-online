6c21f43756f0e73b3b983ca8dd7cfdd6bf976509
Peh <20146907+randomicon00@users.noreply.github.com>
2022-08-29 09:45:03 +0100

    add contact form component

---
+++ front/src/components/Contact.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div id="contact-form">
+      <form>
+        <label>
+          Name:
+          <input type="text" />
+        </label>
+        <label>
+          Email:
+          <input type="email" />
+        </label>
+        <label>
+          Message:
+          <textarea />
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+}
+
