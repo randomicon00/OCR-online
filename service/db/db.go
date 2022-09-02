1aab34e11319ae89efd96aed7604d0f10fdd3565
Peh <20146907+randomicon00@users.noreply.github.com>
2022-09-02 08:31:23 +0100

    add db info to service

--- service/db/db.go
+++ service/db/db.go
@@ -1,7 +1,20 @@
 package main
 
 import (
+  "database/sql"
   "fmt"
+ 
+  // importing sql drivers as the blank identifier
+  // more info: https://www.calhoun.io/why-we-import-sql-drivers-with-the-blank-identifier/
+  _ "github.com/lib/pq"
+)
+
+const (
+  host     = "localhost"
+  port     = 5432
+  user     = "postgres"
+  password = "password"
+  dbname   = "dbname"
 )
 
 func DB() {
