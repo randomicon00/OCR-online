d20a215b5edfb269897da8fd9b076dcb30c4a995
Peh <20146907+randomicon00@users.noreply.github.com>
2022-09-11 10:14:27 +0100

    add comment related to env vars

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -1,6 +1,10 @@
 const hostname = "locahost";
 const port = 8080;
+
+
 // Is a semicolon required after a named export like this one?
+// TODO Most constants here should be replaced with with env variables.
+
 export {
   hostname,
   port,
