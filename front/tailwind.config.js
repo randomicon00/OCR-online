0161f79bb8a4e6f6b7cb30c49d59c985b895e710
Peh <20146907+randomicon00@users.noreply.github.com>
2022-09-15 20:06:00 +0100

    edit tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "540px",
+      md: "720px",
+      lg: "960px",
+      xl: "1140px",
+      "2xl": "1320px",
+    },
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        black: "#212b36",
+        dark: "#090E34",
+        "dark-700": "#090e34b3",
+        primary: "#3056D3",
+        secondary: "#13C296",
+        "body-color": "#637381",
+        warning: "#FBBF24",
+      },
+      boxShadow: {
+        input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
+        pricing: "0px 39px 23px -27px rgba(0, 0, 0, 0.04)",
+        "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
+        testimonial: "0px 60px 120px -20px #EBEFFD",
+      },
+    },
   },
   plugins: [],
 }
