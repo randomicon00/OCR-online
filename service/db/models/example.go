6e3e6c25125cd0ae6645f87b23d831bda2c7ba22
Peh <20146907+randomicon00@users.noreply.github.com>
2022-09-25 10:41:45 +0100

    add field

--- service/db/models/example.go
+++ service/db/models/example.go
@@ -1,6 +1,7 @@
 package models
 
 type Example struct {
-	ID   uint   `json:"id" gorm:"primary_key"`
-	Name string `json:"name"`
+  ID   uint   `json:"id" gorm:"primary_key"`
+  Name string `json:"name"`
+  Age  uint   `json:"age"`
 }
