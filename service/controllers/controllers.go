af41421d75e8db78c9d01d9c3d4865bde37f8257
Peh <20146907+randomicon00@users.noreply.github.com>
2022-09-26 08:20:07 +0100

    add controllers package

---
+++ service/controllers/controllers.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+  //TODO Add more imports
+  
+)
+func Health(c *gin.Context) {
+  c.JSON(http.statusOK, gin.H{"response": "all is ok"})
+}
+
