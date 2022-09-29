10d4401fb3cfec3db13f095ce3c2a932a2274f01
Peh <20146907+randomicon00@users.noreply.github.com>
2022-09-29 09:12:32 +0100

    add db model entry

--- service/db/models/example.go
+++ service/db/models/example.go
@@ -1,7 +1,15 @@
 package models
 
-type Example struct {
-  ID   uint   `json:"id" gorm:"primary_key"`
-  Name string `json:"name"`
-  Age  uint   `json:"age"`
+type User struct {
+  ID          uint   `json:"id" gorm:"primary_key"`
+  Name        string `json:"name"`
+  UserName    string `json:"username"` 
+  Age         uint   `json:"age"`
 }
+
+type Entry struct {
+  ID       uint   `json:"id" gorm:"primary_key"`
+  Uri      string `json:"uri"`
+  Created  string `json:"created"`
+}
+
