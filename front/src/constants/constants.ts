c4b58fc3bbff3bada4ad257cd6cfa17103a4b4e7
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-02 09:46:47 +0100

    edit constants

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -1,9 +1,7 @@
 const hostname = "locahost";
 const port = 8080;
 const maxWidth = 1200;
-const maxHeight = NaN;
 const minWidth = 640;
-const minHeight = 0;
 // Is a semicolon required after a named export like this one?
 // TODO Most constants here should be replaced with with env variables.
 
@@ -11,8 +9,6 @@ export {
   hostname,
   port,
   maxWidth,
-  maxHeight,
   minWidth,
-  minHeight,
 }
 
