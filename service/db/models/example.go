ff66cd094f12fd655e7699054b71404dbfccee4b
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-05 08:56:34 +0100

    add struct

--- service/db/models/example.go
+++ service/db/models/example.go
@@ -1,14 +1,26 @@
 package models
 
+import (
+  "time"
+)
+
 type User struct {
   ID          uint   `json:"id" gorm:"primary_key"`
   UserName    string `json:"username"` 
+  Password    string `json:"password"`
   Age         uint   `json:"age"`
 }
 
 type Entry struct {
-  ID       uint   `json:"id" gorm:"primary_key"`
-  Uri      string `json:"uri"`
-  Created  string `json:"created"`
+  ID         uint      `json:"id" gorm:"primary_key"`
+  Uri        string    `json:"uri"`
+  CreatedAt  time.Time `json:"created_at"`
+  UpdatedAt  time.Time `json:"updated_at"`
+  Status     string 
+}
+
+type Config struct {
+  Address    string
+  Port       string
 }
 
