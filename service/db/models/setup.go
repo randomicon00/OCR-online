8f3ef099ef01fba65f2bbaf622aad0564ac21dff
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-05 09:10:37 +0100

    refactor db connection

--- service/db/models/setup.go
+++ service/db/models/setup.go
@@ -1,20 +1,35 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+  "fmt"
+
+  "gorm.io/driver/postgres"
+  "gorm.io/gorm"  
+)
+
+const (
+   host   = "localhost"
+   user   = "gorm"
+   password = "gorm"
+   dbname = "gorm"
+   port = 9932
+   sslmode = "disabled"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "test.db")
+	
+  psqlInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s" +
+  "sslmode=%s TimeZone=Asia/Pacific",
+  host, user, password, dbname, port sslmode)
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to db")
 	}
 
-	database.AutoMigrate(&Example{})
+	db.AutoMigrate(&Entry{})
 
 	DB = database
 }
