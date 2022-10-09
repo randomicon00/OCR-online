2b38c890c7b6fb745262d181c4a8050cff2e1326
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-09 10:03:42 +0100

    refactor benefits

--- front/src/components/Benefits.tsx
+++ front/src/components/Benefits.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
+const use_online = ``;
+const img_to_txt = ``;
+const pdf_to_doc = ``;
+const iOS_Android = ``;
+const secure_conversion = ``;
+const free_tool = ``;
+
 const benefits = [
-  { title: "use online", text: "paragraph", icon: "icon url here" },
-  { title: "image to text", text: "paragraph", icon: "icon url here" },
-  { title: "convert PDF to DOC", text: "paragraph", icon: "icon url here" },
-  { title: "works with iOS and Android", text: "paragraph", icon: "icon url here" },
-  { title: "secure conversion", text: "paragraph", icon: "icon url here" },
-  { title: "free tool", text: "paragraph", icon: "icon url here" },
+  { title: "use online", text: use_online, icon: "icon url here" },
+  { title: "image to text", text: img_to_txt, icon: "icon url here" },
+  { title: "convert PDF to DOC", text: pdf_to_doc, icon: "icon url here" },
+  { title: "works with iOS and Android", text: iOS_Android, icon: "icon url here" },
+  { title: "secure conversion", text: secure_conversion, icon: "icon url here" },
+  { title: "free tool", text: free_tool, icon: "icon url here" },
 ];
 
 type BenefitsProps = {
