8870fa6143e2b4db7a8265c0c839a6a5266579c6
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-11 08:37:05 +0100

    refactor controllers

--- service/controllers/controllers.go
+++ service/controllers/controllers.go
@@ -11,7 +11,19 @@ func Health(c *gin.Context) {
 }
 
 func GetAll(c *gin.Context) {
-  c.JSON(http.statusOK, gin.H{"response": "got all values!"})
+  c.JSON(http.statusOK, gin.H{"response": "got all entries!"})
+}
+
+func CreateOne(c *gin.Context) {
+  c.JSON(http.statusOK, gin.H{"response": "created one entry!"})
+}
+
+func EditOne(c *gin.Context) {
+  c.JSON(http.statusOK, gin.H{"response": "edited one entry!"})
+}
+
+func DeleteOne(c *gin.Context) {
+  c.JSON(http.statusOK, gin.H{"response": "deleted one entry!"})
 }
 
 //TODO Add all other functions and use authentication for all
