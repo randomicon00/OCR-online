6dafa2a866e8ccd0a7e32a09b7f3f7b436ecf55e
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-12 09:00:01 +0100

    add helper unit function

--- front/src/lib/utils.ts
+++ front/src/lib/utils.ts
@@ -2,6 +2,13 @@ const formatNumber = () => {};
 const formatDate = () => {};
 const formatFileSizeUnit = () => {};
 
+const getUnit = () => {
+  const timeUnits = ["s", "ms", "us"]; 
+  
+  return "";
+};
+
+
 export {
   formatNumber,
   formatDate,
