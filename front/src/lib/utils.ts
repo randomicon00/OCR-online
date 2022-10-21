d25546e22d4b63f13956598943607769fdce783e
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-21 19:55:28 +0100

    add time format

--- front/src/lib/utils.ts
+++ front/src/lib/utils.ts
@@ -4,8 +4,13 @@ const formatFileSizeUnit = () => {};
 
 const getUnit = () => {
   const timeUnits = ["s", "ms", "us"]; 
-  
-  return "";
+  if (Math.floor(num / 1000000) > 0) { 
+    return "s";
+  }
+  if (Math.floor(num / 1000) > 0) { 
+    return "ms"; 
+  }
+  return "us";
 };
 
 
