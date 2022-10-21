8a85d635c5b12228e1a33948ea48fad5d5969360
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-21 20:00:03 +0100

    add log to controller functions

--- service/controllers/controllers.go
+++ service/controllers/controllers.go
@@ -1,34 +1,41 @@
 package controllers
 
 import (
+  "log"
   "net/http"
 
   "github.com/gin-gonic/gin"
 )
 
 func Health(c *gin.Context) {
+  log.Println("Health request");
   c.JSON(http.StatusOK, gin.H{"response": "all is ok"})
 }
 
 func GetAll(c *gin.Context) {
+  log.Println("GetAll request");
   c.JSON(http.StatusOK, gin.H{"response": "got all entries!"})
 }
 
 func CreateOne(c *gin.Context) {
+  log.Println("CreateOne request");
   c.JSON(http.StatusOK, gin.H{"response": "created one entry!"})
 }
 
 func EditOne(c *gin.Context) {
+  log.Println("EditOne request");
   c.JSON(http.StatusOK, gin.H{"response": "edited one entry!"})
 }
 
 func DeleteOne(c *gin.Context) {
+  log.Println("DeleteOne request");
   c.JSON(http.StatusOK, gin.H{"response": "deleted one entry!"})
 }
 
 //TODO Add all other functions and use authentication for all
 
 func GetAllByDate(c *gin.Context) {
+  log.Println("GetAllByDate request");
   c.JSON(http.StatusOK, gin.H{"response": "got all entries by date"})
 }
 
