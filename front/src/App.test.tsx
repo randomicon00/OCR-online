55e76a29f24a0c4b2e473f6e37dc10dcc39cdfed
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-22 21:32:54 +0100

    edit test function name

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders ocr title text', () => {
+it('renders ocr title text', () => {
   render(<App />);
   const titleElement = screen.getByText(/image to text conversion/i);
   expect(titleElement).toBeInTheDocument();
