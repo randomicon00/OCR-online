e77dd0b315da3168462c50ee52fc018001cd20cd
Peh <20146907+randomicon00@users.noreply.github.com>
2022-10-29 09:33:45 +0100

    add state to app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Intro from "./components/Intro";
 import Convert from "./components/Convert";
 import Benefits from "./components/Benefits";
 import Layout from "./components/Layout";
 
 const App = () => {
+  const [state, setState] = useState(0);
+  
   return (
     <div id="container">
     <Layout>
