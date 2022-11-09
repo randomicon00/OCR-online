405406b457b075a3ad89e4d062f5a1ffb8e5015f
Peh <20146907+randomicon00@users.noreply.github.com>
2022-11-09 09:28:59 +0100

    add root element check

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+if (!document.getElementById('root')) {
+  throw new Error('No root element found in DOM!');
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
