1dc1098419417d534092e2008c61dfb48789a466
Peh <20146907+randomicon00@users.noreply.github.com>
2023-03-13 20:01:24 +0100

    edit: small typo

--- front/src/components/Convert.tsx
+++ front/src/components/Convert.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const Convert = () => {
   return (
     <div>
-      <p>convert component...</p>
+      <p>Convert component...</p>
     </div>
   );
 }
 
 export default Convert;
+
