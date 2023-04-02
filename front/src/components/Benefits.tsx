0a3fec899977483a21262087ec1769489d4aa5ab
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-02 20:34:39 +0000

    fix: various fixes

--- front/src/components/Benefits.tsx
+++ front/src/components/Benefits.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { ChatAlt2Icon, DocumentTextIcon, GlobeAltIcon, LockClosedIcon, PhotographIcon, } from '@heroicons/react/solid';
+import {
+  ChatAlt2Icon,
+  DocumentTextIcon,
+  GlobeAltIcon,
+  LockClosedIcon,
+  PhotographIcon,
+} from "@heroicons/react/solid";
 
 const use_online = `Use it online without installing any software`;
 const img_to_txt = `Convert images to editable text`;
@@ -9,12 +15,36 @@ const secure_conversion = `Secure conversion with encryption and automatic delet
 const free_tool = `Completely free to use without any hidden fees or limits`;
 
 const benefits = [
-  { title: "Use online", text: use_online, icon: <GlobeAltIcon className="h-6 w-6" /> },
-  { title: "Image to text", text: img_to_txt, icon: <PhotographIcon className="h-6 w-6" /> },
-  { title: "Convert PDF to DOC", text: pdf_to_doc, icon: <DocumentTextIcon className="h-6 w-6" /> },
-  { title: "Works with iOS and Android", text: iOS_Android, icon: <ChatAlt2Icon className="h-6 w-6" /> },
-  { title: "Secure conversion", text: secure_conversion, icon: <LockClosedIcon className="h-6 w-6" /> },
-  { title: "Free tool", text: free_tool, icon: <PhotographIcon className="h-6 w-6" /> },
+  {
+    title: "Use online",
+    text: use_online,
+    icon: <GlobeAltIcon className="h-6 w-6" />,
+  },
+  {
+    title: "Image to text",
+    text: img_to_txt,
+    icon: <PhotographIcon className="h-6 w-6" />,
+  },
+  {
+    title: "Convert PDF to DOC",
+    text: pdf_to_doc,
+    icon: <DocumentTextIcon className="h-6 w-6" />,
+  },
+  {
+    title: "Works with iOS and Android",
+    text: iOS_Android,
+    icon: <ChatAlt2Icon className="h-6 w-6" />,
+  },
+  {
+    title: "Secure conversion",
+    text: secure_conversion,
+    icon: <LockClosedIcon className="h-6 w-6" />,
+  },
+  {
+    title: "Free tool",
+    text: free_tool,
+    icon: <PhotographIcon className="h-6 w-6" />,
+  },
 ];
 
 type BenefitsProps = {
@@ -33,14 +63,16 @@ const Benefit = ({ title, text, icon }: BenefitsProps) => {
       <p className="mt-2 text-base text-gray-500">{text}</p>
     </div>
   );
-}
+};
 
 const Benefits = () => {
   return (
     <div className="py-12 bg-white">
       <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Benefits</h2>
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            Benefits
+          </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Why choose our service?
           </p>
@@ -49,17 +81,13 @@ const Benefits = () => {
         <div className="mt-10">
           <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
             {benefits.map(({ title, text, icon }) => (
-              <Benefit 
-                title={title} 
-                text={text}
-                icon={icon}
-              />
+              <Benefit title={title} text={text} icon={icon} />
             ))}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Benefits;
--- front/src/components/Icon.tsx
+++ front/src/components/Icon.tsx
@@ -1,7 +1,9 @@
-const Icon = () => {
-  return (
-      <div><i></i></div>
-  );
-}
+type IconProps = {
+  iconUrl: string;
+};
+
+const Icon = ({ iconUrl }: IconProps) => {
+  return <img src={iconUrl} alt="" data-testid="icon" />;
+};
 
 export default Icon;
