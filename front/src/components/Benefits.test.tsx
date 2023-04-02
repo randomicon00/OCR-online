456b4fc801e226eaf4024a44c4c89aa44d7905a2
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-02 20:25:27 +0000

    edit: write tests and descriptions for benefits comp

--- front/src/components/Benefits.test.tsx
+++ front/src/components/Benefits.test.tsx
@@ -1,2 +1,26 @@
-export {}
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Benefits from "./Benefits";
 
+describe("Benefits component", () => {
+  it("renders all benefits with their titles and texts", () => {
+    render(<Benefits />);
+    expect(screen.getByText("Use online")).toBeInTheDocument();
+    expect(screen.getByText("Convert images to editable text")).toBeInTheDocument();
+    expect(screen.getByText("Convert PDF files to editable Word documents")).toBeInTheDocument();
+    expect(screen.getByText("Works on both iOS and Android devices")).toBeInTheDocument();
+    expect(screen.getByText("Secure conversion with encryption and automatic deletion of uploaded files")).toBeInTheDocument();
+    expect(screen.getByText("Completely free to use without any hidden fees or limits")).toBeInTheDocument();
+  });
+
+  it("renders all benefits with their icons", () => {
+    render(<Benefits />);
+    expect(screen.getByTestId("use-online-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("img-to-txt-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("pdf-to-doc-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("ios-android-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("secure-conversion-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("free-tool-icon")).toBeInTheDocument();
+  });
+});
--- front/src/components/Benefits.tsx
+++ front/src/components/Benefits.tsx
@@ -1,46 +1,63 @@
 import React from "react";
+import { ChatAlt2Icon, DocumentTextIcon, GlobeAltIcon, LockClosedIcon, PhotographIcon, } from '@heroicons/react/solid';
 
-const use_online = `put text here...`;
-const img_to_txt = `put text here...`;
-const pdf_to_doc = `put text here...`;
-const iOS_Android = `put text here...`;
-const secure_conversion = `put text here...`;
-const free_tool = `put text here...`;
+const use_online = `Use it online without installing any software`;
+const img_to_txt = `Convert images to editable text`;
+const pdf_to_doc = `Convert PDF files to editable Word documents`;
+const iOS_Android = `Works on both iOS and Android devices`;
+const secure_conversion = `Secure conversion with encryption and automatic deletion of uploaded files`;
+const free_tool = `Completely free to use without any hidden fees or limits`;
 
 const benefits = [
-  { title: "Use online", text: use_online, icon: "icon url here" },
-  { title: "Image to text", text: img_to_txt, icon: "icon url here" },
-  { title: "Convert PDF to DOC", text: pdf_to_doc, icon: "icon url here" },
-  { title: "Works with iOS and Android", text: iOS_Android, icon: "icon url here" },
-  { title: "Secure conversion", text: secure_conversion, icon: "icon url here" },
-  { title: "Free tool", text: free_tool, icon: "icon url here" },
+  { title: "Use online", text: use_online, icon: <GlobeAltIcon className="h-6 w-6" /> },
+  { title: "Image to text", text: img_to_txt, icon: <PhotographIcon className="h-6 w-6" /> },
+  { title: "Convert PDF to DOC", text: pdf_to_doc, icon: <DocumentTextIcon className="h-6 w-6" /> },
+  { title: "Works with iOS and Android", text: iOS_Android, icon: <ChatAlt2Icon className="h-6 w-6" /> },
+  { title: "Secure conversion", text: secure_conversion, icon: <LockClosedIcon className="h-6 w-6" /> },
+  { title: "Free tool", text: free_tool, icon: <PhotographIcon className="h-6 w-6" /> },
 ];
 
 type BenefitsProps = {
   title: string;
   text: string;
-  icon: string; //TODO Replace with: JSX.Element or ReactNode Heroicons!
+  icon: JSX.Element;
 };
 
 const Benefit = ({ title, text, icon }: BenefitsProps) => {
   return (
     <div>
-      <h1><i>{icon}</i>{title}</h1>
-      <p>{text}</p>
+      <div className="flex items-center">
+        {icon}
+        <h1 className="ml-2 text-lg font-medium text-gray-900">{title}</h1>
+      </div>
+      <p className="mt-2 text-base text-gray-500">{text}</p>
     </div>
   );
 }
 
 const Benefits = () => {
   return (
-    <div>
-      {benefits.map(({ title, text, icon }) => (
-        <Benefit 
-          title={title} 
-          text={text}
-          icon={icon}
-        />
-      ))}
+    <div className="py-12 bg-white">
+      <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Benefits</h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Why choose our service?
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+            {benefits.map(({ title, text, icon }) => (
+              <Benefit 
+                title={title} 
+                text={text}
+                icon={icon}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
