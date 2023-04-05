5f49efbf111faec50cc8587b56215cdc273ce3ff
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-05 20:27:42 +0000

    edit: enhance nav contact components

--- front/src/components/Contact.tsx
+++ front/src/components/Contact.tsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // logic to handle form submission
+  };
+
   return (
     <div id="contact-form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" />
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
         <label>
           Email:
-          <input type="email" />
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <label>
           Subject:
-          <input type="text" />
+          <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
         </label>
         <label>
           Message:
-          <textarea />
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
         </label>
         <input type="submit" value="submit" />
       </form>
