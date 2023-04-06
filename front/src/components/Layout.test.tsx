360c30d2dff930ce2aad19cd83daddb7ea5322d8
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-06 21:06:42 +0000

    tests for layout component

--- front/src/components/Layout.test.tsx
+++ front/src/components/Layout.test.tsx
@@ -1,1 +1,26 @@
-export {};
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Layout from "./Layout";
+
+describe("Layout", () => {
+  it("should render children", () => {
+    const { getByText } = render(
+      <Layout>
+        <div>Test</div>
+      </Layout>
+    );
+    expect(screen.getByText("Test")).toBeInTheDocument();
+  });
+
+  it("should render navigation and footer", () => {
+    const { getByText } = render(
+      <Layout>
+        <div>Test</div>
+      </Layout>
+    );
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("OCR")).toBeInTheDocument();
+    expect(screen.ggetByText("Contact")).toBeInTheDocument();
+    expect(screen.ggetByText("© 2023 OCR App")).toBeInTheDocument();
+  });
+});
