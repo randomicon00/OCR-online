6be6423234773ad899d70c3360c7841dc79f2a6e
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-06 21:03:33 +0000

    tests for nav component

--- front/src/components/Nav.test.tsx
+++ front/src/components/Nav.test.tsx
@@ -1,1 +1,21 @@
-export {}
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Nav from "./Nav";
+
+describe("Nav", () => {
+  it("renders a navigation menu with links", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    const homeLink = screen.getByRole("link", { name: "Home" });
+    const ocrLink = screen.getByRole("link", { name: "OCR" });
+    const contactLink = screen.getByRole("link", { name: "Contact" });
+
+    expect(homeLink).toHaveAttribute("href", "/");
+    expect(ocrLink).toHaveAttribute("href", "/ocr");
+    expect(contactLink).toHaveAttribute("href", "/contact");
+  });
+});
