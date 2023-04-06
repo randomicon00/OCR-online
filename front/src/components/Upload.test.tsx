92d38fc67f6f97eaef9763eb2fa844176466fa88
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-06 21:01:22 +0000

    enhance upload component

--- front/src/components/Upload.test.tsx
+++ front/src/components/Upload.test.tsx
@@ -1,1 +1,21 @@
-export {};
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Upload from "./Upload";
+
+test("renders input element with specified name", () => {
+  const name = "test-file";
+  render(<Upload name={name} />);
+  const inputElement = screen.getByLabelText(/upload/i);
+  expect(inputElement).toBeInTheDocument();
+  expect(inputElement).toHaveAttribute("name", name);
+});
+
+test("fires onChange event when file is selected", async () => {
+  const handleChange = jest.fn();
+  render(<Upload name="test-file" onChange={handleChange} />);
+  const file = new File(["(⌐□_□)"], "test.png", { type: "image/png" });
+  const inputElement = screen.getByLabelText(/upload/i);
+  await userEvent.upload(inputElement, file);
+  expect(handleChange).toHaveBeenCalledTimes(1);
+  expect(handleChange).toHaveBeenCalledWith(file);
+});
--- front/src/components/Upload.tsx
+++ front/src/components/Upload.tsx
@@ -1,11 +1,43 @@
+import React, { ChangeEvent, useState } from "react";
+
 type UploadProps = {
   name: string;
+  accept: string;
+  onChange: (file: File | null) => void;
 };
 
-const Upload = ({ name }: UploadProps) => {
+const Upload = ({ name, accept, onChange }: UploadProps) => {
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    setFileName(file ? file.name : "");
+    onChange(file);
+  };
+
   return (
     <div className="p-3 text-xs no-underline">
-      <input id="img-upload" type="file" name={name} />
+      <label htmlFor={name} className="block mb-1 font-medium text-gray-700">
+        Upload an image or PDF
+      </label>
+      <input
+        id={name}
+        type="file"
+        name={name}
+        accept={accept}
+        className="hidden"
+        onChange={handleFileChange}
+      />
+      <div className="flex items-center justify-between w-full px-4 py-2 bg-gray-100 border border-gray-300 rounded-md">
+        <span className="mr-2 text-sm font-medium text-gray-700 truncate">
+          {fileName || "Choose a file..."}
+        </span>
+        <button
+          type="button"
+          className="px-2 py-1 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+          Browse
+        </button>
+      </div>
     </div>
   );
 };
