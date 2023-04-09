9b5ab315b3233448c8d026564bc92167bb94d449
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-09 18:59:17 +0000

    edit: add state to convert component

--- front/src/components/Convert.tsx
+++ front/src/components/Convert.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Convert = () => {
-  return (
-    <div>
-      <p>Convert component...</p>
-    </div>
-  );
+  const [status, setStatus] = useState("idle");
+
+  useEffect(() => {
+    // Simulate a conversion that takes 5 seconds
+    if (status === "progress") {
+      const timeoutId = setTimeout(() => {
+        setStatus("complete");
+      }, 5000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [status]);
+
+  const startConversion = () => {
+    setStatus("progress");
+  };
+
+  const renderContent = () => {
+    if (status === "idle") {
+      return <button onClick={startConversion}>Convert</button>;
+    }
+
+    if (status === "progress") {
+      return <p>Conversion in progress...</p>;
+    }
+
+    if (status === "complete") {
+      return <p>Conversion complete!</p>;
+    }
+  };
+
+  return <div>{renderContent()}</div>;
 };
 
 export default Convert;
