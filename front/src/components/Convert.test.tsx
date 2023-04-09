8c21cee60dd6d1e6044d179ae640bef0f46c415d
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-09 18:59:55 +0000

    edit: add test  convert component

--- front/src/components/Convert.test.tsx
+++ front/src/components/Convert.test.tsx
@@ -1,1 +1,23 @@
-export {};
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Convert from "./Convert";
+
+describe("Convert component", () => {
+  it("should render idle state", () => {
+    render(<Convert />);
+    const idleState = screen.getByText(/convert component/i);
+    expect(idleState).toBeInTheDocument();
+  });
+
+  it("should render progress state", () => {
+    render(<Convert state="progress" />);
+    const progressState = screen.getByText(/converting/i);
+    expect(progressState).toBeInTheDocument();
+  });
+
+  it("should render complete state", () => {
+    render(<Convert state="complete" />);
+    const completeState = screen.getByText(/conversion complete/i);
+    expect(completeState).toBeInTheDocument();
+  });
+});
