1f0d5a0f2ed72ce94638fdc9399f3db4d73be95c
Peh <20146907+randomicon00@users.noreply.github.com>
2023-04-19 20:09:40 +0000

    fix: logic formatting

--- service/logic/logic.go
+++ service/logic/logic.go
@@ -1,59 +1,59 @@
 package logic
 
 import (
-  "log"
+	"log"
 
 	"github.com/randomicon00/OCR-Webservice-frontend/service/db/models"
 )
 
 func GetAll() ([]models.Entry, error) {
-  log.Println("GetAll request")
-  db := models.DB
-  var entries []models.Entry
-  result := db.Find(&entries)
-  if result.Error != nil {
-    return nil, result.Error 
-  }
+	log.Println("GetAll request")
+	db := models.DB
+	var entries []models.Entry
+	result := db.Find(&entries)
+	if result.Error != nil {
+		return nil, result.Error
+	}
 
-  return entries, nil 
+	return entries, nil
 }
 
 func GetAllByDate(date string) (interface{}, error) {
-  log.Println("GetAllByDate request")
+	log.Println("GetAllByDate request")
 
-  // implementation here
+	// implementation here
 
-  return nil, nil 
+	return nil, nil
 }
 
 func CreateOne(item interface{}) (interface{}, error) {
-  log.Println("CreateOne request")
+	log.Println("CreateOne request")
 
-  // implementation here
+	// implementation here
 
-  return nil, nil
+	return nil, nil
 }
 
 func EditOne(id string) (interface{}, error) {
-  log.Println("EditOne request")
+	log.Println("EditOne request")
 
-  // implementation here
+	// implementation here
 
-  return nil, nil
+	return nil, nil
 }
 
 func DeleteOne(id string) (interface{}, error) {
-  log.Println("DeleteOne request")
+	log.Println("DeleteOne request")
 
-  // implementation here
+	// implementation here
 
-  return nil, nil
+	return nil, nil
 }
 
 func GetStats() (interface{}, error) {
-  log.Println("GetStats request")
+	log.Println("GetStats request")
 
-  // implementation here
+	// implementation here
 
-  return nil, nil
+	return nil, nil
 }
