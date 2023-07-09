f1685437a653de58ef79e36a37b63ba488b54b93
Peh <20146907+randomicon00@users.noreply.github.com>
2023-07-09 10:03:26 +0100

    feat: added test for Contact comp

--- front/src/components/Contact.test.tsx
+++ front/src/components/Contact.test.tsx
@@ -1,1 +1,42 @@
-export {};
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Contact from './Contact';
+
+describe('Contact Form', () => {
+  test('renders contact form', () => {
+    render(<Contact />);
+    const form = screen.getByTestId('contact-form');
+    expect(form).toBeInTheDocument();
+  });
+
+  test('updates on input', () => {
+    render(<Contact />);
+
+    const inputName = screen.getByLabelText(/Name:/i);
+    userEvent.type(inputName, 'John Doe');
+    expect(inputName).toHaveValue('John Doe');
+
+    const inputEmail = screen.getByLabelText(/Email:/i);
+    userEvent.type(inputEmail, 'john@example.com');
+    expect(inputEmail).toHaveValue('john@example.com');
+
+    const inputSubject = screen.getByLabelText(/Subject:/i);
+    userEvent.type(inputSubject, 'Hello');
+    expect(inputSubject).toHaveValue('Hello');
+
+    const textareaMessage = screen.getByLabelText(/Message:/i);
+    userEvent.type(textareaMessage, 'Test message');
+    expect(textareaMessage).toHaveValue('Test message');
+  });
+
+  test('submits the form', () => {
+    const handleSubmit = jest.fn();
+    render(<Contact onSubmit={handleSubmit} />);
+    
+    const button = screen.getByRole('button', { name: /submit/i });
+    userEvent.click(button);
+
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
+  });
+});
