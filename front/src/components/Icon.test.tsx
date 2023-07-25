4c142b7383abb3d678c6bc2484ab3de2253871c0
Peh <20146907+randomicon00@users.noreply.github.com>
2023-07-25 07:11:05 +0100

    feat: add Icon comp test

--- front/src/components/Icon.test.tsx
+++ front/src/components/Icon.test.tsx
@@ -1,1 +1,12 @@
-export {};
+import React from 'react';
+import { render } from '@testing-library/react';
+import Icon from './Icon';
+
+describe('Icon Component', () => {
+  it('renders with given iconUrl', () => {
+    const { getByTestId } = render(<Icon iconUrl="test-url" />);
+    const img = getByTestId('icon');
+    expect(img).toHaveAttribute('src', 'test-url');
+  });
+});
+
