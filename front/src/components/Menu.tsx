2cb47eeea9be4455716b514d01c52c3e192f4832
Peh <20146907+randomicon00@users.noreply.github.com>
2023-08-05 09:11:39 +0100

    fix: menu display

--- front/src/components/Menu.tsx
+++ front/src/components/Menu.tsx
@@ -2,7 +2,9 @@ const Menu = () => {
   return (
     <div>
       <ul>
-        <li>Menu section</li>
+        <li>Menu Item 1</li>
+        <li>Menu Item 2</li>
+        <li>Menu Item 3</li>
       </ul>
     </div>
   );
