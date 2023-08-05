8be877b1a1d09bc328bb6bccc4e203b8b330d210
Peh <20146907+randomicon00@users.noreply.github.com>
2023-08-05 09:09:57 +0100

    feat: add tests

--- front/src/components/Menu.test.tsx
+++ front/src/components/Menu.test.tsx
@@ -1,1 +1,29 @@
-export {};
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Menu from './Menu'; // Assuming the component file is named Menu.js
+
+describe('Menu component', () => {
+  it('renders without crashing', () => {
+    render(<Menu />);
+  });
+
+  it('displays the menu section', () => {
+    render(<Menu />);
+    const menuSectionElement = screen.getByText('Menu section');
+    expect(menuSectionElement).toBeInTheDocument();
+  });
+
+  it('contains an unordered list', () => {
+    render(<Menu />);
+    const unorderedListElement = screen.getByRole('list');
+    expect(unorderedListElement).toBeInTheDocument();
+  });
+
+  it('contains a list item with the text "Menu section"', () => {
+    render(<Menu />);
+    const listItemElement = screen.getByText('Menu section');
+    expect(listItemElement).toBeInTheDocument();
+  });
+
+  // Add more tests if needed based on the functionality of the Menu component
+});
