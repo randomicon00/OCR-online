6255f440e610879b7aee2280ee7cb8a41727fd41
Peh <20146907+randomicon00@users.noreply.github.com>
2023-08-06 11:34:15 +0100

    edit: enhance test

--- front/src/components/Layout.test.tsx
+++ front/src/components/Layout.test.tsx
@@ -3,24 +3,25 @@ import { render, screen } from "@testing-library/react";
 import Layout from "./Layout";
 
 describe("Layout", () => {
-  it("should render children", () => {
+  beforeEach(() => {
     render(
       <Layout>
         <div>Test</div>
       </Layout>
     );
+  });
+
+  it("should render children", () => {
     expect(screen.getByText("Test")).toBeInTheDocument();
   });
 
-  it("should render navigation and footer", () => {
-    render(
-      <Layout>
-        <div>Test</div>
-      </Layout>
-    );
-    expect(screen.getByText("Home")).toBeInTheDocument();
-    expect(screen.getByText("OCR")).toBeInTheDocument();
-    expect(screen.getByText("Contact")).toBeInTheDocument();
+  it("should render navigation links", () => {
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "OCR" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Contact" })).toBeInTheDocument();
+  });
+
+  it("should render the footer text", () => {
     expect(screen.getByText("© 2023 OCR App")).toBeInTheDocument();
   });
 });
