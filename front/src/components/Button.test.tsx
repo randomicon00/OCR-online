78586bf9a6cd4f5a2bb2cb0331f00a344d84743e
Peh <20146907+randomicon00@users.noreply.github.com>
2023-08-07 10:20:17 +0100

    edit: remove test comments

--- front/src/components/Button.test.tsx
+++ front/src/components/Button.test.tsx
@@ -6,7 +6,6 @@ describe('Button Component', () => {
   it('renders correctly', () => {
     const { getByRole } = render(<Button name="testButton" text="Test Button"/>);
 
-    // Check if the button is in the document.
     const button = getByRole('button');
     expect(button).toBeInTheDocument();
   });
@@ -14,14 +13,12 @@ describe('Button Component', () => {
   it('displays correct text', () => {
     const { getByText } = render(<Button name="testButton" text="Test Button"/>);
 
-    // Check if the button's text is correct.
     expect(getByText('Test Button')).toBeInTheDocument();
   });
 
   it('has correct name attribute', () => {
     const { getByRole } = render(<Button name="testButton" text="Test Button"/>);
 
-    // Check if the button's name attribute is correct.
     const button = getByRole('button');
     expect(button.getAttribute('name')).toBe('testButton');
   });
@@ -29,7 +26,6 @@ describe('Button Component', () => {
   it('has correct style', () => {
     const { getByRole } = render(<Button name="testButton" text="Test Button"/>);
 
-    // Check if the button has correct style.
     const button = getByRole('button');
     expect(button.style.backgroundColor).toBe('green');
   });
@@ -38,10 +34,8 @@ describe('Button Component', () => {
     const handleClick = jest.fn();
     const { getByRole } = render(<Button name="testButton" text="Test Button" onClick={handleClick}/>);
 
-    // Simulate click event.
     fireEvent.click(getByRole('button'));
 
-    // Check if the click event has been triggered.
     expect(handleClick).toHaveBeenCalled();
   });
 });
