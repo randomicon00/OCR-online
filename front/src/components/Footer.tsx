6af02984035a33efc7b530781dfd1aae25157b0a
Peh <20146907+randomicon00@users.noreply.github.com>
2023-08-10 23:32:47 +0100

    fix: add menu items footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -3,7 +3,12 @@ const Footer = () => {
     <footer>
       <div className="footer">
         <div className="footer-menu">
-          <ul><li>Menu item 1 (example)</li></ul>
+          <ul>
+           <li>Menu item 1 (example)</li>
+           <li>Menu item 2 (example)</li>
+           <li>Menu item 3 (example)</li>
+           <li>Menu item 4 (example)</li>
+          </ul>
         </div>
       </div>
       <p>&copy; 2023 OCR Online. All rights reserved.</p>
