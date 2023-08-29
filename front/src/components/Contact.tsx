fd690ae7219568d05ee326a38c0353d1ea35844e
Peh <20146907+randomicon00@users.noreply.github.com>
2023-08-29 08:08:19 +0100

    edit: initial states

--- front/src/components/Contact.tsx
+++ front/src/components/Contact.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 const Contact = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [subject, setSubject] = useState();
+  const [message, setMessage] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
