6d95cfffbe8b5a89922be34863e77d20a6fbb26c
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-09-20 14:12:34 -0400

    edit: replaced tabs

--- service/db/models/models.go
+++ service/db/models/models.go
@@ -3,28 +3,28 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
-	gorm.Model
-	Username string `json:"username"`
+  gorm.Model
+  Username string `json:"username"`
   Email string `json:"email"`
   Password string `json:"password"`
 }
 
 // Conversion represents the model for a conversion
 type Conversion struct {
-	gorm.Model
-	Amount     float64
-	From       string
-	To         string
-	Result     float64
-	Success    bool
+  gorm.Model
+  Amount     float64
+  From       string
+  To         string
+  Result     float64
+  Success    bool
 }
 
 type Config struct {
-	Address string
-	Port    string
-	TLS     bool
+  Address string
+  Port    string
+  TLS     bool
 }
 
 type ConversionError struct {
-	Message string
+  Message string
 }
