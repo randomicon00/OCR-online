59a38c2b5d013eedf7a791fbdf7a33781b6f6c46
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-09-20 14:12:15 -0400

    edit: change title

--- front/src/components/Intro.tsx
+++ front/src/components/Intro.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Intro = () => {
   return (
     <div id="intro">
-      <h1>OCR Image to Text Converter Online</h1>
+      <h1>Online OCR Image to Text Converter</h1>
       <p>
       Image to Text converter enables you to extract text from images or 
         convert PDFs to Doc, Excel, or Text formats using online Optical Character Recognition (OCR) software.
