b99737eea2cd026cdce5a19093a68f7961ace72d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-09-24 13:12:29 -0400

    edit: replace tabs with two spaces

--- service/ocr.go
+++ service/ocr.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/otiai10/gosseract/v2"
+  "fmt"
+  "github.com/otiai10/gosseract/v2"
 )
 
 // TODO should be a library and not an bin
 func main() {
-	client := gosseract.NewClient()
-	defer client.Close()
-	client.SetImage("./images/english.jpeg")
-	text, _ := client.Text()
-	fmt.Println(text)
+  client := gosseract.NewClient()
+  defer client.Close()
+  client.SetImage("./images/english.jpeg")
+  text, _ := client.Text()
+  fmt.Println(text)
 }
