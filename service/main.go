f354660fbe6ac164176d41db77795313b535c30e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-09-24 13:11:57 -0400

    edit: replace tabs with two spaces

--- service/main.go
+++ service/main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"log"
-	"net/http"
+  "log"
+  "net/http"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/randomicon00/OCR-Webservice-frontend/service/db/models"
+  "github.com/gin-contrib/cors"
+  "github.com/gin-gonic/gin"
+  "github.com/randomicon00/OCR-Webservice-frontend/service/db/models"
 )
 
 func SetupRouter() *gin.Engine {
-	r := gin.Default()
-	return r
+  r := gin.Default()
+  return r
 }
 
 func main() {
-	r := SetupRouter()
+  r := SetupRouter()
 
-	// TODO Add the right import and understand how it works
-	models.ConnectDatabase()
+  // TODO Add the right import and understand how it works
+  models.ConnectDatabase()
 
-	router.Use(cors.Default())
-	// TODO Router should be in its own file/package
-	r.POST("/upload", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "upload success!",
-		})
-	})
+  router.Use(cors.Default())
+  // TODO Router should be in its own file/package
+  r.POST("/upload", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "upload success!",
+    })
+  })
 
-	// TODO Move callback to controller
-	r.GET("/heatlh", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "OCR service status: Online",
-		})
-	})
+  // TODO Move callback to controller
+  r.GET("/heatlh", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "OCR service status: Online",
+    })
+  })
 
-	// TODO Move callback to controller
-	r.GET("/result", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "my ocr text!",
-		})
-	})
+  // TODO Move callback to controller
+  r.GET("/result", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "my ocr text!",
+    })
+  })
 
-	log.Println("Starting OCR web service on port 8080...")
-	r.Run(":8080")
+  log.Println("Starting OCR web service on port 8080...")
+  r.Run(":8080")
 }
