d6818947f7e5c8d56017606fbd8008a04254e1a7
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-09-24 13:10:33 -0400

    edit: replace tabs with two spaces

--- service/db/models/setup.go
+++ service/db/models/setup.go
@@ -1,35 +1,35 @@
 package models
 
 import (
-	"fmt"
+  "fmt"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+  "gorm.io/driver/postgres"
+  "gorm.io/gorm"
 )
 
 const (
-	host     = "localhost"
-	user     = "user"
-	password = "password"
-	dbname   = "database"
-	port     = 9932
-	sslmode  = "disabled"
+  host     = "localhost"
+  user     = "user"
+  password = "password"
+  dbname   = "database"
+  port     = 9932
+  sslmode  = "disabled"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	psqlInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=Asia/Pacific",
-		host, user, password, dbname, port, sslmode)
-	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+  psqlInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=Asia/Pacific",
+    host, user, password, dbname, port, sslmode)
+  db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
 
-	if err != nil {
-		panic("Error: Failed to connect to db")
-	}
+  if err != nil {
+    panic("Error: Failed to connect to db")
+  }
 
-	db.AutoMigrate(&Entry{})
+  db.AutoMigrate(&Entry{})
 
-	DB = db
+  DB = db
 }
 
