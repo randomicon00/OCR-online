a683e9a6400afc731fdef23eee082260068c66b7
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-13 13:29:39 +0100

    fix: replace the placeholder nav with real one

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div>
-      <nav>Here goes the header and navigation</nav>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 };
