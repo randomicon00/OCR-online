924314dd6a92fb993fa593281210f509edd83ec9
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-13 13:39:09 +0100

    feat: add test for Header comp

--- front/src/components/Header.test.tsx
+++ front/src/components/Header.test.tsx
@@ -1,1 +1,18 @@
-export {};
+import React from 'react';
+import { render } from '@testing-library/react';
+import Header from './Header'; // Adjust the import according to your file structure
+import Nav from './Nav'; // Adjust the import according to your file structure
+import '@testing-library/jest-dom/extend-expect';
+
+// TODO: Add explanation for the following code.
+jest.mock('./Nav', () => {
+  return function DummyNav() {
+    return <div data-testid="nav">Nav Component</div>;
+  };
+});
+
+test('renders Nav component', () => {
+  const { getByTestId } = render(<Header />);
+  expect(getByTestId('nav')).toBeInTheDocument();
+});
+
