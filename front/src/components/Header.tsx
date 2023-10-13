51752ca800a20e4f7060169bfae5badfe942a541
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-13 13:37:00 +0100

    fix: replace with nav comp instead

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,15 +1,10 @@
 import { Link } from "react-router-dom";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+      <Nav />
     </div>
   );
 };
