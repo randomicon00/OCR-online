bec0e8e7df40bdf711e97e7bcb907c7f931bb43d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2023-10-23 17:55:51 +0100

    edit: small text changes

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
         <div className="footer-menu">
           <ul>
            <li>Home</li>
-           <li>Convert</li>
+           <li>Convert to Text</li>
            <li>About</li>
            <li>Contact</li>
           </ul>
         </div>
       </div>
-      <p>&copy; 2023 OCR Online. All rights reserved.</p>
+      <p>&copy; 2023 Online OCR. All rights reserved.</p>
     </footer>
 
   );
