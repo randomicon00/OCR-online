74e0410f5239194f8144dc69cc26cf0e43df8071
Peh <20146907+randomicon00@users.noreply.github.com>
2023-11-14 19:53:19 +0100

    fix: remove unneeded comment

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -4,8 +4,7 @@ import App from "./App";
 
 it("renders OCR application's intro text", () => {
   render(<App />);
-
-  // Get the h1 element that is the intro element
+  
   const introElement = screen.getByText(/online ocr image to text converter/i);
   
   expect(introElement).toBeInTheDocument();
