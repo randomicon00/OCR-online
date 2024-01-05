aa9c7a799381ce2af40d2d8e46bba5f566b3df64
Peh <20146907+randomicon00@users.noreply.github.com>
2024-01-05 18:09:04 +0100

    fix: remove unused state

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,7 +5,6 @@ import Benefits from "./components/Benefits";
 import Layout from "./components/Layout";
 
 const App = () => {
-  const [state, setState] = useState(0);
   const [show, setShow] = useState(false);
   const refEl = useRef(null);
 
@@ -27,7 +26,7 @@ const App = () => {
 
     // Cleanup at the end of the lifecycle
     return () => {
-      el.removeEventListener("click", handleClick);
+      el.removeEventListener("click",1 handleClick);
     };
   }, []);
 
